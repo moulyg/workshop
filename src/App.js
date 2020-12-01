de9085be9728a6a7e17739cf7c66309806e66557
moulyg <moulyg@gmail.com>
2020-12-01 17:33:25 +0530

    Components and Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
+import React from 'react';
 import './App.css';
 
-function getGreeting(user) {
-    if (user) {
-        return <h1>Hello, {user}!</h1>;
+function WelcomeFn(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+class WelcomeCls extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
     }
-    return <h1>Hello, Stranger.</h1>;
 }
 
 function App() {
-
-
-    const name = 'Mouly Guanrathne';
-    const title = '<h1>mouly</h1><img src="" onerror="alert(\'ad\')" />'
     return (<>
-        <h1>Hello, {name}</h1>
-        <h1>Hello, {getGreeting(name)}</h1>
-        <h1>Hello, {getGreeting(null)}</h1>
-        <a href='#' title={name}>name</a>
-        <div>{title}</div>
-        {/*<div dangerouslySetInnerHTML={{__html: title}} />*/}
+        <WelcomeFn name="Kandy Tuskers"/>
+        <WelcomeCls name="Galle Gladiators"/>
     </>);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, ICITR2020!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
