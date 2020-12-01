1a03683a0493610535f5f48e9e19938bf0f4f169
moulyg <moulyg@gmail.com>
2020-12-01 23:24:14 +0530

    Handling Events

--- src/App.js
+++ src/App.js
@@ -1,113 +1,39 @@
 import React from 'react';
 import './App.css';
 
+class App extends React.Component {
 
-class LifeCycle extends React.Component {
-
-    constructor(props) {
-        super(props);
-        console.log("constructor");
+    constructor() {
+        super();
         this.state = {
             count: 0
         }
+        this.updateState = this.updateState.bind(this);
     }
 
-    static getDerivedStateFromProps() {
-        console.log("getDerivedStateFromProps");
-        return null;
+    clickEvent() {
+        alert('Foo')
     }
 
-    componentDidMount() {
-        console.log("componentDidMount");
-    }
-
-    componentDidUpdated() {
-        console.log("componentDidMount");
-    }
-
-    componentWillUnmount() {
-        console.log("componentWillUnmount");
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("shouldComponentUpdate");
-        if (nextState.count === 4) {
-            return false;
-        }
-        return  true;
-    }
-
-
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log("getSnapshotBeforeUpdate", prevProps, prevState);
-        return prevState.count;
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log("componentDidUpdate", prevProps, prevState, snapshot);
-    }
-
-    render() {
-        console.log("render");
-        return <div>
-            <div>
-                <div>
-
-                    <span style={{width: '100px', display: 'block'}}>Count:</span>
-
-                    <small>{this.state.count}</small>
-                </div>
-                <hr/>
-            </div>
-            <button onClick={() => this.setState({count: this.state.count + 1})}>Add</button>
-            <button onClick={() => this.forceUpdate()}>Force Update</button>
-        </div>;
-    }
-}
-
-class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            show: true,
-            title: 'LPL'
-        }
-        this.updateStateDirectly = this.updateStateDirectly.bind(this);
-    }
-
-    updateStateDirectly() {
-        this.state.show = false;
+    clickWithArgument(text) {
+        alert(text)
     }
 
     updateState() {
-        // Wrong
-        this.setState({
-            show: !this.state.show,
-        });
-
-        // Correct
-        /*this.setState((state, props) => ({
-            show: !state.show
-        }));*/
+        this.setState((state, props) => ({
+            count: state.count + 1
+        }));
     }
+
 
     render() {
         return <div>
-            <h1>{this.state.title}</h1>
-            <hr />
-            {this.state.show && <LifeCycle/>}
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <button onClick={() => this.updateState()}>{this.state.show ? 'Hide': 'Show'}</button>
-
-            <br />
-            <hr />
-            Show: {this.state.show ? 'True': 'False'}
-            <hr />
-            <button onClick={() => this.updateStateDirectly()}>Update State Directly</button>
+            <button onClick={this.clickEvent}>Click Event</button>
+            <hr/>
+            <button onClick={this.updateState}>Update State ({this.state.count})</button>
+            <hr/>
+            <button onClick={() => this.clickWithArgument('2020')}>Delete Row</button>
+            <button onClick={this.clickWithArgument.bind(this, '2024')}>Delete Row</button>
         </div>;
     }
 }
