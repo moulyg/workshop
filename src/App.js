9c8475d7ae5c535c3d110c54481edf7649a7ef41
moulyg <moulyg@gmail.com>
2020-12-01 23:52:22 +0530

    Lists and Keys

--- src/App.js
+++ src/App.js
@@ -3,44 +3,27 @@ import './App.css';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
-    }
-
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
-    }
-
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
-    }
-    greeting(props) {
-        const isLoggedIn = props.isLoggedIn;
-        if (isLoggedIn) {
-            return <h1>Hi, User</h1>;
+    constructor() {
+        super();
+        this.state = {
+            numbers: [1, 2, 3, 4, 5]
         }
-        return <h1>Hi Guest</h1>;
+        this.add = this.add.bind(this);
+    }
+
+    add() {
+        this.setState((state) => ({
+            numbers: [...state.numbers, state.numbers.length + 1]
+        }))
     }
 
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        let button;
-        if (isLoggedIn) {
-            button = <button onClick={this.handleLogoutClick}>Log Out</button>;
-        } else {
-            button = <button onClick={this.handleLoginClick}>Login</button>;
-        }
-
-        return (
-            <div>
-                {this.greeting(this.state)}
-                {button}
-                {isLoggedIn && <p>This is secured area</p>}
-            </div>
-        );
+        const numbers = this.state.numbers;
+        return (<>
+                {numbers.map((number) => <li key={number.toString()}>{number}</li>)}
+                <button onClick={this.add}>Add</button>
+            </>
+        )
     }
 }
 
