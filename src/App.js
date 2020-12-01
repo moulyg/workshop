8616bcdeecbe46d751c809c6ca1138f2557f6a23
moulyg <moulyg@gmail.com>
2020-12-02 00:35:31 +0530

    hoc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
 
-    const getTds = () => {
-        return (
-            <React.Fragment>
-                <td>1</td>
-                <td>2</td>
-            </React.Fragment>
-        )
+function withBackGround(WrappedComponent) {
+    return class extends React.Component {
+
+        render() {
+            return <WrappedComponent className='App-header' {...this.props} />;
+        }
     }
+}
+
+function WrappedComponent(props) {
+    return <div {...props}>WrappedComponent</div>;
+}
+
+function App() {
+    const NewComponent = withBackGround(WrappedComponent);
+
     return (
         <div>
-            <table>
-                <tr>{getTds()}</tr>
-            </table>
+            <NewComponent/>
         </div>
     );
 }
