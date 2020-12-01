ce1cac519459a9aa8c555f345f25adc289e4aab9
moulyg <moulyg@gmail.com>
2020-12-02 00:20:44 +0530

    State Effects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-    // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
+    const [clicks, setClicks] = useState(0);
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        document.title = `You clicked ${clicks} times & count ${count}`;
+    });
+
+    // Similar to componentDidMount
+    useEffect(() => {
+        document.title = `Page Loaded`;
+    }, []);
+
+    // Similar to componentDidMount
+    /* useEffect(() => {
+        document.title = `Capture clicks only clicks: ${clicks}, count: ${count}`;
+    }, [clicks]);*/
 
     return (
         <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
+            <p>You clicked {clicks} times</p>
+            <p>You count {count} times</p>
+            <button onClick={() => setClicks(clicks + 1)}>
                 Click me
+            </button>
+            <button onClick={() => setCount(count + 1)}>
+                Count
             </button>
         </div>
     );
