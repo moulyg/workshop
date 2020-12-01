3818541e94a628e0b1c9395661da5549db647a31
moulyg <moulyg@gmail.com>
2020-12-02 00:25:04 +0530

    Fragment

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [clicks, setClicks] = useState(0);
 
-    // Similar to componentDidMount and componentDidUpdate:
-    useEffect(() => {
-        document.title = `You clicked ${clicks} times & count ${count}`;
-    });
-
-    // Similar to componentDidMount
-    useEffect(() => {
-        document.title = `Page Loaded`;
-    }, []);
-
-    // Similar to componentDidMount
-    /* useEffect(() => {
-        document.title = `Capture clicks only clicks: ${clicks}, count: ${count}`;
-    }, [clicks]);*/
-
+    const getTds = () => {
+        return (
+            <React.Fragment>
+                <td>1</td>
+                <td>2</td>
+            </React.Fragment>
+        )
+    }
     return (
         <div>
-            <p>You clicked {clicks} times</p>
-            <p>You count {count} times</p>
-            <button onClick={() => setClicks(clicks + 1)}>
-                Click me
-            </button>
-            <button onClick={() => setCount(count + 1)}>
-                Count
-            </button>
+            <table>
+                <tr>{getTds()}</tr>
+            </table>
         </div>
     );
 }
